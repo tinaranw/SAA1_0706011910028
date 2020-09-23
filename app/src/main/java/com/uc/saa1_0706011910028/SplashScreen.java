4732221ba5cd135a0c108eefcedf7ae2a775d9fa
Tinara Nathania Wiryonoputro <tinaranathania@gmail.com>
2020-09-24 02:06:58 +0700

    so far :>

--- app/src/main/java/com/uc/saa1_0706011910028/SplashScreen.java
+++ app/src/main/java/com/uc/saa1_0706011910028/SplashScreen.java
@@ -2,13 +2,29 @@ package com.uc.saa1_0706011910028;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,Starter.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
--- app/src/main/java/com/uc/saa1_0706011910028/Starter.java
+++ app/src/main/java/com/uc/saa1_0706011910028/Starter.java
@@ -2,13 +2,56 @@ package com.uc.saa1_0706011910028;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Starter extends AppCompatActivity {
+
+    ImageButton addLecturer, addCourse, loginStudent, registerStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter);
+
+        addLecturer = findViewById(R.id.addLecturerStarterBtn);
+        addCourse = findViewById(R.id.addCourseStarterBtn);
+        loginStudent = findViewById(R.id.loginStudentStarterBtn);
+        registerStudent = findViewById(R.id.addStudentStarterBtn);
+
+        addLecturer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Starter.this, AddLecturer.class);
+                startActivity(intent);
+            }
+        });
+
+        addCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Starter.this, AddCourse.class);
+                startActivity(intent);
+            }
+        });
+
+        loginStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Starter.this, LoginStudent.class);
+                startActivity(intent);
+            }
+        });
+
+        registerStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Starter.this, StudentRegister.class);
+                startActivity(intent);
+            }
+        });
     }
 }
