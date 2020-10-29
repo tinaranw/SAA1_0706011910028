b591c9cfa8e6eeadc83ada91a531292af4c6b8b2
Tinara Nathania Wiryonoputro <tinaranathania@gmail.com>
2020-10-29 18:06:55 +0700

    fragmentadapter

---
+++ app/src/main/java/com/uc/saa1_0706011910028/adapter/ScheduleFragmentAdapter.java
@@ -0,0 +1,16 @@
+package com.uc.saa1_0706011910028.adapter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uc.saa1_0706011910028.R;
+
+public class ScheduleFragmentAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_fragment_adapter);
+    }
+}
